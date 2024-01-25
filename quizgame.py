d60c66eebafb019ff168206cea2a1573ef5bb1ea
VictoryVicy <obed.noni99@gmail.com>
2024-01-25 22:23:40 +0700

    In Progress

--- quizgame.py
+++ quizgame.py
@@ -1,3 +1,33 @@
 print("Selamat datang di Quiz Game")
 
-playing = input("Apakah kamu ingin bermain?")
+bermain = input("Apakah kamu ingin bermain? (Main/tidak) ")
+
+if bermain.lower() != "Main":
+    quit()
+
+print("Oke, mari kita mulai!")
+
+jawaban = input("Siapa presiden indonesia sekarang ")
+if jawaban == "Joko Widodo":
+    print("Selamat, kamu benar!")
+else:
+    print("Maaf, kamu salah!")
+
+jawaban = input("Siapa calon presiden Indonesia tahun 2024 nomor 1? ")
+if jawaban == "Anies Baswedan":
+    print("Selamat, kamu benar!")
+else:
+    print("Maaf, kamu salah!")
+
+jawaban = input("Siapa calon presiden Indonesia tahun 2024 nomor 2? ")
+if jawaban == "Prabowo Subianto":
+    print("Selamat, kamu benar!")
+else:
+    print("Maaf, kamu salah!")
+
+jawaban = input("Siapa calon presiden Indonesia tahun 2024 nomor 3? ")
+if jawaban == "Ganjar Pranowo":
+    print("Selamat, kamu benar!")
+else:
+    print("Maaf, kamu salah!")
+
